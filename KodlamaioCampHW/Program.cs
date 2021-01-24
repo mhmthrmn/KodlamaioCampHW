3c3765b7930595a553a0eda32e5519de20692663
Mehmet HARMAN <mehmet_harman@aof.anadolu.edu.tr>
2021-01-24 17:11:34 +0300

    Add project files.

---
+++ KodlamaioCampHW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KodlamaioCampHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
