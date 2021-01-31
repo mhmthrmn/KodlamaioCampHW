d5f968222d7cc765ca06a871778e0409c5fa3294
Mehmet HARMAN <mehmet_harman@aof.anadolu.edu.tr>
2021-02-01 00:14:37 +0300

    GamerProject HW

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Bir bankada müşteri takibi yapmak istiyorsunuz.
+             Musteri isimli bir Class oluşturunuz.Müşteriye istediğiniz özellikleri ekleyiniz. (Id, Ad, Soyad....)
+                MusteriManager sınıfı oluşturunuz.Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz.*/
+        }
+    }
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserVAlidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 1985,
+                FirstName = "mehmet",
+                LastName = "harman",
+                IdentityNumber = 12345
+            });
+        }
+    }
+}
